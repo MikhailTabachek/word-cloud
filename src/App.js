063b14a164d6ab99a1fd32fb381e20438535ccc9
Mikhail <mishanyaveselyi@gmail.com>
2022-11-13 23:20:36 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Word Cloud!</h1>
+        <main>
+          <div className='cloud-space'>
+          <h1>  Cloud will be here! </h1> 
+            
+            </div>
+
+        </main>
       </header>
     </div>
   );
